a893b14e66c8d3dce43d9cc0828ca235e222e329
saiani1 <saiani@naver.com>
2024-07-24 14:47:08 +0900

    BlogItem컴포넌트 레이아웃 변경

--- src/shared/ui/BlogItem.tsx
+++ src/shared/ui/BlogItem.tsx
@@ -1,22 +1,42 @@
-export const BlogItem = () => {
+import Image from "next/image";
+import Hui from "public/assets/image/hui2.jpg";
+import { Hashtag } from "./Hashtag";
+import SideArrowIcon from "public/assets/svg/right-top-arrow-icon.svg";
+
+export const BlogItem = ({
+  data,
+  direction,
+}: {
+  data?: any;
+  direction?: "row" | "column";
+}) => {
   return (
-    <li className="w-[280px] p-[20px] bg-slate-200/50 rounded-[5px]">
-      <button
-        type="button"
-        className="flex flex-col items-center gap-y-[15px]"
-      >
-        <div className="w-full h-[180px] bg-slate-300/50 overflow-hidden rounded-[5px]">
-          {/* <img src={profileImage} /> */}
+    <li className="w-[289px]">
+      <button type="button" className="flex flex-col items-start text-left">
+        <div className="w-[289px] overflow-hidden rounded-[5px]">
+          <Image src={Hui} height={190} alt="후잇" />
         </div>
-        <div className="flex flex-col text-start gap-y-[5px] px-[4px]">
-          <h3 className="leading-tight text-[17px] font-semibold text-[#555]">Pariatur incididunt cillum enim exercitation.</h3>
-          <p
-            className="leading-snug text-[14px] text-[#666] line-clamp-2"
-          >
-            Deserunt qui quis nostrud in commodo anim mollit proident quis irure mollit ex exercitation. Mollit reprehenderit anim ipsum ullamco laborum minim tempor. Esse cillum magna est consequat in mollit ipsum Lorem aliqua ipsum quis sint sunt tempor. Velit laboris magna est et anim.
+        <span className="mt-[15px] mb-[10px] text-[13px] text-black-888">
+          2024.07.23
+        </span>
+        <div className="flex gap-x-[10px]">
+          <p className="mb-[10px] text-[20px] text-black-444 font-semibold leading-6">
+            Conversation With London Makr & Co.
           </p>
+          <div className="flex shrink-0 justify-center items-center w-[20px] h-[20px]">
+            <SideArrowIcon />
+          </div>
         </div>
+        <span className="text-[15px] text-black-555 leading-5">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </span>
+        <ul className="flex gap-x-[3px] mt-[15px]">
+          <Hashtag tagName="Design" size="s" />
+          <Hashtag tagName="Programming" size="s" />
+          <Hashtag tagName="Interview" size="s" />
+        </ul>
       </button>
     </li>
-  )
-}
+  );
+};

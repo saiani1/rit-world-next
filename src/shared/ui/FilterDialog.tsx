48676459416549b7af73e92c7497858c3ae0b94e
saiani1 <saiani@naver.com>
2024-07-24 17:35:59 +0900

    FilterDialog 사이즈 조절

--- src/shared/ui/FilterDialog.tsx
+++ src/shared/ui/FilterDialog.tsx
@@ -10,15 +10,18 @@ const RadioLabel = ({
   checked?: boolean;
 }) => {
   return (
-    <div className="flex items-center gap-x-[5px] py-[7px] pl-[10px]">
+    <div className="flex items-center gap-x-[7px] py-[6px] pl-[3px]">
       <input
         type="radio"
-        className="appearance-none w-[8px] h-[8px] border border-black-FFF rounded-full shadow-commonShadow checked:bg-blue-100"
+        className="appearance-none w-[14px] h-[14px] border-[2px] border-black-FFF rounded-full shadow-commonShadow checked:bg-blue-100"
         id={id}
         name={group}
         defaultChecked={checked}
       />
-      <label htmlFor={id} className="text-[12px] text-black-888 font-medium">
+      <label
+        htmlFor={id}
+        className="pt-[1px] text-[13px] text-black-888 font-medium"
+      >
         {name}
       </label>
     </div>
@@ -27,8 +30,8 @@ const RadioLabel = ({
 
 export const FilterDialog = () => {
   return (
-    <div className="absolute right-0 bottom-[-70px] flex justify-center items-center w-[80px] h-[67px] bg-black-50 rounded-[5px] before:-translate-x-1/2 before:content-[''] before:w-0 before:h-0 before:border-[4px] before:border-transparent before:border-b-black-50 before:absolute before:top-[-8px] before:right-[5px]">
-      <ul className="w-full p-[5px] divide-y divide-black-FFF">
+    <div className="absolute right-0 bottom-[-75px] z-10 flex justify-center items-center w-[85px] h-[70px] bg-black-50 rounded-[5px] before:-translate-x-1/2 before:content-[''] before:w-0 before:h-0 before:border-[4px] before:border-transparent before:border-b-black-50 before:absolute before:top-[-8px] before:right-[5px]">
+      <ul className="w-full p-[10px] divide-y divide-black-FFF">
         <li>
           <RadioLabel name="전체" id="all" group="filter" checked />
         </li>

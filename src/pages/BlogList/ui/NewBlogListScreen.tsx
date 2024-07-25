57103f632c9d5d3e565768f6628e6a11a8140e26
saiani1 <saiani@naver.com>
2024-07-25 17:33:20 +0900

    최신 포스트 화면에 인기 해시태그 리스트 추가

--- src/pages/BlogList/ui/NewBlogListScreen.tsx
+++ src/pages/BlogList/ui/NewBlogListScreen.tsx
@@ -1,33 +1,95 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import ArrowIcon from "public/assets/svg/top-arrow-icon.svg";
 import {
   BlogItem,
   BigBlogItem,
   FilterButton,
   Title,
   WriteButton,
+  Hashtag,
 } from "shared/index";
 
-export const NewBlogListScreen = () => {
+const HashtagTrends = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <div>
-      <div className="flex justify-between items-center pb-[15px] mb-[10px]">
-        <Title name="최신 포스트" />
+    <div className="border-b-[2px] border-black-EEE mb-[35px] pb-[20px]">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-x-[15px]">
+          <Title name="인기 해시태그" />
+          <button
+            type="button"
+            className={`flex justify-center items-center w-[20px] h-[20px] rounded-[2px] bg-black-EEE`}
+            onClick={handleClick}
+          >
+            <motion.div
+              animate={{ rotate: isOpen ? 180 : 0 }}
+              transition={{ duration: 0.25 }}
+            >
+              <ArrowIcon className="rotate-180" />
+            </motion.div>
+          </button>
+        </div>
         <div className="flex gap-x-[13px]">
           <WriteButton />
           <FilterButton />
         </div>
       </div>
-      <ul className="flex flex-wrap justify-between gap-y-[40px]">
-        <li>
-          <BigBlogItem />
-        </li>
-        <li>
-          <ul className="flex flex-col gap-y-[25px]">
-            <BlogItem direction="row" />
-            <BlogItem direction="row" />
-            <BlogItem direction="row" />
-          </ul>
-        </li>
-      </ul>
+      {isOpen && (
+        <ul className="flex flex-wrap gap-x-[5px] gap-y-[10px] mt-[20px]">
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+          <Hashtag tagName="Degisn" size="m" />
+        </ul>
+      )}
     </div>
+  );
+};
+
+export const NewBlogListScreen = () => {
+  return (
+    <>
+      <div>
+        <HashtagTrends />
+      </div>
+      <div>
+        <div className="flex justify-between items-center pb-[15px] mb-[10px]">
+          <Title name="최신 포스트" />
+        </div>
+        <ul className="flex flex-wrap justify-between gap-y-[40px]">
+          <li>
+            <BigBlogItem />
+          </li>
+          <li>
+            <ul className="flex flex-col gap-y-[25px]">
+              <BlogItem direction="row" />
+              <BlogItem direction="row" />
+              <BlogItem direction="row" />
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };

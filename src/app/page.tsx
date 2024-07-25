1fbaffce1300a6dfecbfdfc69abc34cf9ea1119b
saiani1 <saiani@naver.com>
2024-07-25 17:09:36 +0900

    필터 관련 코드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-'use client'
-import { BlogListScreen } from "pages/BlogList";
+"use client";
+import { filterAtom } from "entities/blog/model/atom";
+import { useAtomValue } from "jotai";
+import { AllBlogListScreen, NewBlogListScreen } from "pages/BlogList";
 
 const HomePage = () => {
+  const filterName = useAtomValue(filterAtom);
+
   return (
-    <BlogListScreen />
+    <>{filterName === "all" ? <AllBlogListScreen /> : <NewBlogListScreen />}</>
   );
-}
+};
 
 export default HomePage;
--- src/shared/ui/FilterDialog.tsx
+++ src/shared/ui/FilterDialog.tsx
@@ -1,14 +1,22 @@
+import { filterAtom } from "entities/blog/model/atom";
+import { useAtom } from "jotai";
+
 const RadioLabel = ({
   name,
   id,
   group,
-  checked,
 }: {
   name: string;
   id: string;
   group: string;
-  checked?: boolean;
 }) => {
+  const [filterName, setFilterName] = useAtom(filterAtom);
+
+  const handleChange = (e: any) => {
+    const id = e.target.id;
+    setFilterName(id);
+  };
+
   return (
     <div className="flex items-center gap-x-[7px] py-[6px] pl-[3px]">
       <input
@@ -16,7 +24,8 @@ const RadioLabel = ({
         className="appearance-none w-[14px] h-[14px] border-[2px] border-black-FFF rounded-full shadow-commonShadow checked:bg-blue-100"
         id={id}
         name={group}
-        defaultChecked={checked}
+        defaultChecked={filterName === id}
+        onChange={handleChange}
       />
       <label
         htmlFor={id}
@@ -33,7 +42,7 @@ export const FilterDialog = () => {
     <div className="absolute right-0 bottom-[-75px] z-10 flex justify-center items-center w-[85px] h-[70px] bg-black-50 rounded-[5px] before:-translate-x-1/2 before:content-[''] before:w-0 before:h-0 before:border-[4px] before:border-transparent before:border-b-black-50 before:absolute before:top-[-8px] before:right-[5px]">
       <ul className="w-full p-[10px] divide-y divide-black-FFF">
         <li>
-          <RadioLabel name="전체" id="all" group="filter" checked />
+          <RadioLabel name="전체" id="all" group="filter" />
         </li>
         <li>
           <RadioLabel name="최신순" id="new" group="filter" />

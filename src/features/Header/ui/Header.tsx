b8bde7f210c43cf1a31a47d5dde55e7e900e4fb3
saiani1 <saiani@naver.com>
2024-07-25 17:10:50 +0900

    활성화된 메뉴명에 배경색 & 애니메이션 추가

--- src/features/Header/ui/Header.tsx
+++ src/features/Header/ui/Header.tsx
@@ -1,26 +1,30 @@
 "use client";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import { useAtom } from "jotai";
+import { motion } from "framer-motion";
 
+import logo from "public/assets/logo.png";
+import { DarkmodeToggle } from "./DarkmodeToggle";
 import { HEADER_ARR } from "features/Header/lib/constants";
 import { loginAtom } from "entities/user/model/atom";
-import logo from "public/assets/logo.png";
-import Image from "next/image";
-import { DarkmodeToggle } from "./DarkmodeToggle";
 
 export const Header = () => {
   const router = useRouter();
   const [isLogin, setIsLogin] = useAtom(loginAtom);
+  const [isActive, setIsActive] = useState("BLOG");
   const handleClick = () => router.push("/");
 
   const handleClickHeader = (e: React.MouseEvent<HTMLUListElement>) => {
     const name = (e.target as HTMLButtonElement).name;
-    if (name === "LOG OUT") {
-      // logOutAPI().then(() => {
-      //   toast.success("로그아웃 되었습니다.")
-      //   setIsLogin(false);
-      // });
-    } else if (name === "LOG IN") router.push("/signin");
+    setIsActive(name);
+    // if (name === "LOG OUT") {
+    //   logOutAPI().then(() => {
+    //     toast.success("로그아웃 되었습니다.")
+    //     setIsLogin(false);
+    //   });
+    // } else if (name === "LOG IN") router.push("/signin");
   };
 
   return (
@@ -33,21 +37,29 @@ export const Header = () => {
         </button>
         <nav>
           <ul
-            className="flex items-center gap-x-[30px]"
+            className="flex items-center gap-x-[3px]"
             onClick={handleClickHeader}
           >
             {HEADER_ARR.map((header) => (
               <li key={header.id}>
-                <button
+                <motion.button
                   type="button"
-                  className="text-[17px] text-[#777] font-semibold"
+                  className={`${isActive === header.title ? "bg-green-500 text-black-FFF" : "text-black-777"} px-[15px] py-[2px] rounded-[5px] text-[17px] font-semibold`}
                   name={header.id === 1 && !isLogin ? "LOG IN" : header.title}
+                  initial={{
+                    backgroundColor: "##FFFFFF",
+                  }}
+                  animate={{
+                    backgroundColor:
+                      isActive === header.title ? "#05AAAA" : "#FFFFFF",
+                  }}
+                  transition={{ duration: 0.3 }}
                 >
                   {header.id === 1 && !isLogin ? "LOG IN" : header.title}
-                </button>
+                </motion.button>
               </li>
             ))}
-            <li>
+            <li className="ml-[15px]">
               <DarkmodeToggle />
             </li>
           </ul>

bb3dc9622f6e6ee78715d051aee9d757ca37aeb9
saiani1 <saiani@naver.com>
2025-04-05 18:01:19 +0900

    RegisterInput props에 rest추가

--- src/shared/ui/RegisterInput.tsx
+++ src/shared/ui/RegisterInput.tsx
@@ -4,9 +4,10 @@ import { forwardRef } from "react";
 type RegisterInputType = React.ComponentProps<"input">;
 
 export const RegisterInput = forwardRef<HTMLInputElement, RegisterInputType>(
-  ({}, ref) => {
+  ({ ...rest }, ref) => {
     return (
       <input
+        {...rest}
         ref={ref}
         className="w-64 px-5 py-1.5 border rounded-full border-slate-300 placeholder:text-xs focus:outline-slate-400"
       />

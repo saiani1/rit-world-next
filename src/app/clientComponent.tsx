56715b98e1ea1e158d1b05549dc9998750f4d0ec
saiani1 <saiani@naver.com>
2025-04-05 21:59:20 +0900

    프로필데이터 api호출로직 작성

--- src/app/clientComponent.tsx
+++ src/app/clientComponent.tsx
@@ -1,11 +1,26 @@
 "use client";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
 import { Category } from "features/Category";
 import { Header } from "features/Header";
-import { ProfileAside } from "features/Profile";
-import { usePathname } from "next/navigation";
+import { ProfileAside, ProfileType } from "features/Profile";
+import { supabase } from "shared/index";
 
 const ClientComponent = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
+  const [profileData, setProfileData] = useState<ProfileType>();
+
+  useEffect(() => {
+    (async () => {
+      let { data: user, error } = await supabase
+        .from("user")
+        .select("*")
+        .single();
+      if (error) console.log("프로필 로드에 실패했습니다.");
+      else setProfileData(user);
+    })();
+  }, []);
 
   if (pathname === "/signin") return <>{children}</>;
 
@@ -14,7 +29,7 @@ const ClientComponent = ({ children }: { children: React.ReactNode }) => {
       <Header />
       <div className="flex justify-between w-[80%] mt-[10px] mb-[40px] gap-x-[10px] overflow-hidden">
         <div className="flex flex-col">
-          <ProfileAside />
+          {profileData && <ProfileAside data={profileData} />}
           <Category />
         </div>
         <div className="pt-[105px] px-[50px] pb-[40px] w-full bg-white rounded-xl overflow-scroll">

2490b88c4567630ae6d5a33a0b4dbc21e35aadae
saiani1 <saiani@naver.com>
2025-04-05 18:01:38 +0900

    불필요한 타입 제거

--- src/entities/user/model/type.ts
+++ src/entities/user/model/type.ts
@@ -2,8 +2,3 @@ export type SignInUserInfoType = {
   userId: string;
   password: string;
 };
-
-export type SignUpUserInfoType = SignInUserInfoType & {
-  confirm_password: string;
-  nickname: string;
-};

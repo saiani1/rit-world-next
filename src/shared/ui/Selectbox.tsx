3a8c2a677a93a9eee798e9ca972d7eb845ce8a2a
saiani1 <saiani@naver.com>
2025-04-16 15:18:54 +0900

    Selectbox컴포넌트 더미데이터 제거

--- src/shared/ui/Selectbox.tsx
+++ src/shared/ui/Selectbox.tsx
@@ -5,7 +5,7 @@ import ArrowIcon from "public/assets/svg/top-arrow-icon.svg";
 import { useOnClickOutside } from "shared/hooks/useOnClickOutside";
 
 type SelectboxType = {
-  data?: [];
+  data: [];
   placeholder: string;
 };
 
@@ -13,8 +13,6 @@ export const Selectbox = ({ data, placeholder }: SelectboxType) => {
   const selectRef = useRef(null);
   const [isClick, setIsClick] = useState(false);
   const [selectedOption, setSelectedOption] = useState(placeholder);
-
-  const tmpArr = ["러바오", "아이바오", "푸바오", "루이바오", "후이바오"];
 
   const handleClick = () => {
     setIsClick((prev) => !prev);
@@ -45,7 +43,7 @@ export const Selectbox = ({ data, placeholder }: SelectboxType) => {
           className="absolute top-[39px] left-0 text-center w-full z-10 bg-black-F5 text-black-999 text-[13px] font-medium divide-y divide-black-FFF rounded-[5px]"
           onClick={handleClickOption}
         >
-          {tmpArr.map((item, i) => (
+          {data.map((item, i) => (
             <li key={`select-${i}`}>
               <button type="button" name={item} className="h-[34px] w-full">
                 {item}

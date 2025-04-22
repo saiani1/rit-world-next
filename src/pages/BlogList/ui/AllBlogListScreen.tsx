0bc08253988d85e8e2122e24f58ebc5341a5a7bc
saiani1 <saiani@naver.com>
2025-04-22 21:33:58 +0900

    SSR과 클라이언트의 조건부 렌더링 불일치 해결

--- src/pages/BlogList/ui/AllBlogListScreen.tsx
+++ src/pages/BlogList/ui/AllBlogListScreen.tsx
@@ -1,21 +1,26 @@
 "use client";
-import { useAtomValue } from "jotai";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 import { BlogItem, FilterButton, WriteButton } from "features/Blog";
-import { loginAtom } from "entities/user";
 import { Title } from "shared/ui";
 
 const AllBlogListScreen = () => {
-  const isLogin = useAtomValue(loginAtom);
+  const isLogin = Cookies.get("login") === "Y";
+  const [isClient, setIsClient] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <div>
       <div className="flex justify-between items-center pb-[15px] mb-[10px]">
         <Title name="전체 포스트" />
         <div className="flex gap-x-[13px]">
-          {isLogin && <WriteButton />}
+          {isLogin && isClient && <WriteButton />}
           <FilterButton />
         </div>
       </div>

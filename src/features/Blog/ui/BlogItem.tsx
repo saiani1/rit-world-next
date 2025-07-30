9f9561af90e0572dc4e52abc2b89a83eaf14ef0a
saiani1 <saiani@naver.com>
2025-07-30 22:29:07 +0900

    BlogItem컴포넌트 더미데이터 -> 실제 데이터로 변경

--- src/features/Blog/ui/BlogItem.tsx
+++ src/features/Blog/ui/BlogItem.tsx
@@ -1,68 +1,44 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import dayjs from "dayjs";
 
-import Hui from "public/assets/image/hui2.jpg";
-import Rui from "public/assets/image/rui.jpg";
-import SideArrowIcon from "public/assets/svg/right-top-arrow-icon.svg";
 import { Hashtag } from "shared/ui";
+import { BlogType } from "../model";
 
 type BlogItemType = {
-  data?: any;
-  direction?: "row" | "column";
+  data: BlogType;
 };
 
-export const BlogItem = ({ data, direction }: BlogItemType) => {
-  const height = direction === "row" ? 154 : 190;
-
+export const BlogItem = ({ data }: BlogItemType) => {
   return (
-    <li className={`${direction === "row" ? "" : "w-[250px]"}`}>
+    <li>
       <Link
-        href="_"
-        className={`flex ${direction === "row" ? "flex-row gap-x-[15px]" : "flex-col"} items-start text-left`}
+        href={data.path}
+        className={`flex flex-col items-start w-[250px] text-left`}
       >
-        <div
-          className={`${direction === "row" ? "shrink-0 w-[240px] h-[145px]" : "w-[250px] h-[150px]"} relative overflow-hidden rounded-[5px]`}
-        >
+        <div className="w-full h-[150px] relative overflow-hidden rounded-[5px]">
           <Image
-            src={direction === "row" ? Rui : Hui}
-            style={{ objectFit: "cover" }}
+            src={data.thumbnail}
+            style={{ objectFit: "cover", objectPosition: "top" }}
             fill
-            alt="후잇"
+            alt={data.subject}
           />
         </div>
-        <div className={`${direction === "row" ? "w-[225px]" : "mt-[15px]"}`}>
-          <span
-            className={`${direction === "row" ? "mb-[8px]" : "mb-[8px]"} inline-block text-[13px] text-black-888`}
-          >
-            2024.07.23
+        <div className="mt-[15px] w-full">
+          <span className="mb-[8px] inline-block text-[13px] text-black-888">
+            {dayjs(data.create_at).format("YYYY.MM.DD")}
           </span>
-          <div
-            className={`${direction === "row" ? "gap-x-[5px] mb-[2px]" : "gap-x-[10px]"} flex`}
-          >
-            <p
-              className={`${direction === "row" ? "mb-[5px] text-[17px] leading-5" : "mb-[6px] text-[19px] leading-6"} text-black-444 font-semibold`}
-            >
-              Conversation With London Makr & Co.
-            </p>
-            <div
-              className={`flex shrink-0 justify-center items-center ${direction === "row" ? "mt-[3px] w-[14px] h-[14px]" : "mt-[2px] w-[18px] h-[18px]"}`}
-            >
-              <SideArrowIcon className="fill-black-999" />
-            </div>
-          </div>
-          <span
-            className={`${direction === "row" ? "text-[12px] leading-4" : "text-[13px] leading-5"} text-black-777 line-clamp-2`}
-          >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. printing and typesetting industry.
+          <p className="mb-[6px] text-[19px] leading-6 text-black-444 font-semibold">
+            {data.subject}
+          </p>
+          <span className="text-[13px] leading-5 text-black-777 line-clamp-2">
+            {data.summary}
           </span>
-          <ul
-            className={`flex gap-x-[3px] ${direction === "row" ? "mt-[13px]" : "mt-[15px]"} `}
-          >
-            <Hashtag name="Design" size="s" />
-            <Hashtag name="Programming" size="s" />
-            <Hashtag name="Interview" size="s" />
+          <ul className="flex gap-x-[3px] gap-y-[4px] mt-[15px] w-full flex-wrap">
+            {data.blog_hashtag?.map((hash) => (
+              <Hashtag key={hash.hashtag_id.id} name={hash.hashtag_id.name} />
+            ))}
           </ul>
         </div>
       </Link>

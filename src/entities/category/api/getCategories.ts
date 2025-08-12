5bf94545a0d2fe524bef2c44a2fce94174871a4a
saiani1 <saiani@naver.com>
2025-08-12 19:20:15 +0900

    api호출코드 에러처리 추가

--- src/entities/category/api/getCategories.ts
+++ src/entities/category/api/getCategories.ts
@@ -1,13 +1,16 @@
 "use server";
 import { createClient } from "shared/api/server";
-import { CategoryType } from "../model";
 
-export const getCategories = async (): Promise<CategoryType[]> => {
-  const supabase = await createClient();
-  const { data, error } = await supabase.from("category").select("*");
+export const getCategories = async () => {
+  try {
+    const supabase = await createClient();
+    const { data, error } = await supabase.from("category").select("*");
 
-  if (error || !data) {
-    throw new Error("Failed to fetch categories");
+    if (error || !data) {
+      throw new Error("Failed to fetch categories");
+    }
+    return data;
+  } catch (err) {
+    console.error(err);
   }
-  return data;
 };

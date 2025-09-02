32d60bbf6eb683d1fe9663ce5c1ccf68c9c9004b
saiani1 <saiani@naver.com>
2025-09-02 19:12:24 +0900

    Category필터링 최적화

--- src/features/Category/ui/Category.tsx
+++ src/features/Category/ui/Category.tsx
@@ -7,16 +7,32 @@ import { BiSubdirectoryRight } from "react-icons/bi";
 
 import { CategoryType } from "entities/category";
 import { CategoryListAtom } from "../model";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
-type CategoryComType = {
+type CategoryProps = {
   data: CategoryType[];
 };
 
-export const Category = ({ data }: CategoryComType) => {
+export const Category = ({ data }: CategoryProps) => {
   const searchParams = useSearchParams();
   const categoryId = searchParams?.get("category");
   const setCategoryList = useSetAtom(CategoryListAtom);
+  const { topLevelCategories, childCategoriesMap } = useMemo(() => {
+    const topLevelCategories: CategoryType[] = [];
+    const childCategoriesMap = new Map<string, CategoryType[]>();
+
+    data.forEach((category) => {
+      if (category.parent_id === null) {
+        topLevelCategories.push(category);
+      } else {
+        const children = childCategoriesMap.get(category.parent_id) || [];
+        children.push(category);
+        childCategoriesMap.set(category.parent_id, children);
+      }
+    });
+
+    return { topLevelCategories, childCategoriesMap };
+  }, [data]);
 
   useEffect(() => {
     setCategoryList(data);
@@ -24,48 +40,43 @@ export const Category = ({ data }: CategoryComType) => {
 
   return (
     <nav className="relative flex flex-col grow mt-[10px] py-[20px] px-[32px] w-[250px] bg-white rounded-xl">
-      <p className="pb-[4px] mb-[20px] text-[#888] text-[12px] border-b">
+      <p className="pb-[4px] mb-[20px] text-black-888 text-[12px] border-b">
         CATEGORY
       </p>
       <ul className="flex flex-col gap-y-2">
         <li>
           <Link
             href={{ pathname: "/" }}
-            className={`flex items-center gap-x-2 ${!categoryId ? "text-purple-100 font-medium" : "text-black-777 font-regular"}`}
+            className={`flex items-center gap-x-2 ${!categoryId ? "text-purple-100 font-medium" : "text-black-777 font-normal"}`}
           >
             <IoDocumentTextOutline size={16} />
             <span>All</span>
           </Link>
         </li>
-        {data?.map((cate) => {
-          if (cate.parent_id === null)
-            return (
-              <li key={cate.id} className="flex flex-col gap-y-1">
-                <Link
-                  href={{ pathname: "/", query: { category: cate.id } }}
-                  className={`flex items-center gap-x-2 ${categoryId === cate.id ? "text-purple-100 font-medium" : "text-black-777 font-regular"}`}
-                >
-                  <IoDocumentTextOutline size={16} />
-                  <span>{cate.title}</span>
-                </Link>
-                <ul className="flex flex-col gap-y-1">
-                  {data
-                    .filter((item) => item.parent_id === cate.id)
-                    .map((item) => (
-                      <li key={item.id}>
-                        <Link
-                          href={{ pathname: "/", query: { category: item.id } }}
-                          className={`flex items-center gap-x-2 ml-2 ${categoryId === item.id ? "text-purple-100 font-medium" : "text-black-777 font-regular"}`}
-                        >
-                          <BiSubdirectoryRight />
-                          <span>{item.title}</span>
-                        </Link>
-                      </li>
-                    ))}
-                </ul>
-              </li>
-            );
-        })}
+        {topLevelCategories.map((cate) => (
+          <li key={cate.id} className="flex flex-col gap-y-1">
+            <Link
+              href={{ pathname: "/", query: { category: cate.id } }}
+              className={`flex items-center gap-x-2 ${categoryId === cate.id ? "text-purple-100 font-medium" : "text-black-777 font-normal"}`}
+            >
+              <IoDocumentTextOutline size={16} />
+              <span>{cate.title}</span>
+            </Link>
+            <ul className="flex flex-col gap-y-1">
+              {childCategoriesMap.get(cate.id)?.map((item) => (
+                <li key={item.id}>
+                  <Link
+                    href={{ pathname: "/", query: { category: item.id } }}
+                    className={`flex items-center gap-x-2 ml-2 ${categoryId === item.id ? "text-purple-100 font-medium" : "text-black-777 font-normal"}`}
+                  >
+                    <BiSubdirectoryRight />
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
       </ul>
     </nav>
   );

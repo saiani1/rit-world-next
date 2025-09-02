b313e28c2f763cb5834f4400c9ee05b1d3de56d6
saiani1 <saiani@naver.com>
2025-09-02 16:35:08 +0900

    수정/삭제 버튼 클릭 시 모달이 뜨도록 로직 추가

--- src/features/Blog/ui/ButtonWrap.tsx
+++ src/features/Blog/ui/ButtonWrap.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
+import { useSetAtom } from "jotai";
 import { TbEdit } from "react-icons/tb";
 import { FaRegTrashAlt } from "react-icons/fa";
 
+import { ModalAtom } from "features/Modal";
 import { deleteBlog } from "entities/blog";
-import { CommonButton } from "shared/ui";
 
 type ButtonWrapType = {
   id: string;
@@ -12,9 +13,18 @@ type ButtonWrapType = {
 
 export const ButtonWrap = ({ id }: ButtonWrapType) => {
   const router = useRouter();
+  const setModalData = useSetAtom(ModalAtom);
   const { blog } = useParams() as { blog: string };
 
-  const handleClickDeleteButton = async () => {
+  const handleClickDeleteButton = () => {
+    setModalData({
+      title: "블로그 삭제",
+      description: "해당 블로그를 삭제하시겠습니까?",
+      confirm: () => callDeleteBlog,
+    });
+  };
+
+  const callDeleteBlog = async () => {
     const params = {
       blog_id: id,
       path: blog,
@@ -26,21 +36,33 @@ export const ButtonWrap = ({ id }: ButtonWrapType) => {
     }
   };
 
+  const handleClickEditButton = () => {
+    setModalData({
+      title: "블로그 수정",
+      description: "해당 블로그를 수정하시겠습니까?",
+      confirm: () => router.push(`/edit/${blog}`),
+    });
+  };
+
   return (
     <ul className="flex gap-x-[4px] items-center">
       <li>
-        <Link href={`/edit/${blog}`} className="p-1">
+        <Link
+          href={`/${blog}/confirm`}
+          className="p-1"
+          onClick={handleClickEditButton}
+        >
           <TbEdit size={20} stroke="#777" />
         </Link>
       </li>
       <li>
-        <CommonButton className="p-1">
-          <FaRegTrashAlt
-            size={17}
-            fill="#777"
-            onClick={handleClickDeleteButton}
-          />
-        </CommonButton>
+        <Link
+          href={`/${blog}/confirm`}
+          className="p-1"
+          onClick={handleClickDeleteButton}
+        >
+          <FaRegTrashAlt size={17} fill="#777" />
+        </Link>
       </li>
     </ul>
   );

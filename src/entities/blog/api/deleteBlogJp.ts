798c28c2d6b92facf80d38004ceed047d3e208bf
saiani1 <saiani@naver.com>
2025-09-09 20:23:43 +0900

    에러 console.error로 수정 & 콘솔은 주석처리

--- src/entities/blog/api/deleteBlogJp.ts
+++ src/entities/blog/api/deleteBlogJp.ts
@@ -4,7 +4,9 @@ type deleteBlogJpType = {
   blog_id: string;
 };
 
-export const deleteBlogJp = async ({ blog_id }: deleteBlogJpType) => {
+export const deleteBlogJp = async ({
+  blog_id,
+}: deleteBlogJpType): Promise<boolean> => {
   try {
     // blog_translation_hashtag제거
     const { error: hashtagError } = await supabase
@@ -12,10 +14,10 @@ export const deleteBlogJp = async ({ blog_id }: deleteBlogJpType) => {
       .delete()
       .eq("blog_id", blog_id);
     if (hashtagError) {
-      console.log("hashtagError", hashtagError);
+      console.error("hashtagError", hashtagError);
       return false;
     }
-    console.log("blog_translation_hashtag제거 완료");
+    // console.log("blog_translation_hashtag제거 완료");
 
     // blog_translation테이블의 row제거
     const { error: blogError } = await supabase
@@ -23,13 +25,14 @@ export const deleteBlogJp = async ({ blog_id }: deleteBlogJpType) => {
       .delete()
       .eq("id", blog_id);
     if (blogError) {
-      console.log("blogError", blogError);
+      console.error("blogError", blogError);
       return false;
     }
-    console.log("blog_translation테이블의 row제거 완료");
+    // console.log("blog_translation테이블의 row제거 완료");
 
     return true;
   } catch (err) {
     console.error(err);
+    return false;
   }
 };

d3dc9c4264763293bce38cbc2027d4779dbe6a85
saiani1 <saiani@naver.com>
2025-10-22 18:47:31 +0900

    Hashtag구조 수정

--- src/shared/ui/Hashtag.tsx
+++ src/shared/ui/Hashtag.tsx
@@ -8,13 +8,11 @@ type HashtagType = React.ComponentProps<"button"> & {
 
 export const Hashtag = ({ page, className, ...rest }: HashtagType) => {
   return (
-    <li>
-      <CommonButton
-        className={`${className} ${page ? "px-[14px] py-[2px] border border-white/60 font-semibold text-white cursor-default" : "px-[10px] bg-purple-50 text-purple-100"} flex items-center rounded-full whitespace-nowrap h-[24px] text-[13px]`}
-        {...rest}
-      >
-        {rest.name}
-      </CommonButton>
-    </li>
+    <CommonButton
+      className={`${className} ${page ? "px-[14px] py-[2px] border border-white/60 font-semibold text-white cursor-default" : "px-[10px] bg-purple-50 text-purple-100"} flex items-center rounded-full whitespace-nowrap h-[24px] text-[13px]`}
+      {...rest}
+    >
+      {rest.name}
+    </CommonButton>
   );
 };

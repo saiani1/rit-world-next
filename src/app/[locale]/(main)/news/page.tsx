713a907f504e74bf9c45e179759d69028e9d38ce
saiani1 <saiani@naver.com>
2025-11-18 21:03:24 +0900

    안쓰는 props삭제

--- src/app/[locale]/(main)/news/page.tsx
+++ src/app/[locale]/(main)/news/page.tsx
@@ -4,7 +4,7 @@ import { getReleaseNotes, getReleaseNotesJP } from "entities/releaseNote";
 const NewsPage = async ({
   params: { locale },
 }: {
-  params: { locale: string; blog: string };
+  params: { locale: string };
 }) => {
   const releaseNoteData =
     locale === "ko" ? await getReleaseNotes() : await getReleaseNotesJP();

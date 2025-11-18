7e8ff30bdf33d41eac870efbb4399be690eaea38
saiani1 <saiani@naver.com>
2025-11-18 20:57:54 +0900

    news페이지 추가

---
+++ src/page/News/ui/NewsScreen.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useTranslations } from "next-intl";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+import { ReleaseNoteType } from "entities/releaseNote";
+import { Title } from "shared/ui";
+
+dayjs.extend(relativeTime);
+dayjs.locale("en");
+
+type NewsScreenType = {
+  data: ReleaseNoteType[];
+};
+
+const NewsScreen = ({ data }: NewsScreenType) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const t = useTranslations("ReleaseNote");
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <>
+      {isMounted && (
+        <div className="mx-[20px] sm:mx-0">
+          <Title name={t("title")} />
+          <ul className="flex flex-col gap-y-4 mt-[20px]">
+            {data.map((item) => {
+              const isNew = dayjs().diff(dayjs(item.update_at), "day") <= 7;
+
+              return (
+                <li
+                  key={item.id}
+                  className={`relative px-7 py-5 rounded-md ${isNew ? "" : "border border-black-DDD"}`}
+                >
+                  {isNew && (
+                    <span className="absolute -inset-[1px] border-[3px] border-yellow-400 rounded-md animate-pulse pointer-events-none" />
+                  )}
+                  <h3 className="mb-[8px] text-[20px] text-black-555 font-semibold">
+                    {item.version}
+                  </h3>
+                  <div className="flex items-center gap-x-2 mb-[16px]">
+                    <div
+                      className={`px-2 py-[2px] text-white font-medium text-[12px] ${item.type === "ADDED" ? "bg-green-500" : "bg-purple-100"} rounded-sm`}
+                    >
+                      {item.type}
+                    </div>
+                    <span className="text-black-999 text-[12px]">
+                      {dayjs(item.update_at).fromNow()}
+                    </span>
+                  </div>
+                  <p className="text-[14px] text-black-777 leading-6">
+                    {item.description}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default NewsScreen;

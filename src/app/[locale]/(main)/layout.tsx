f6afcdd4ed9eb927c1cedc4f96fe6633206f9bb0
saiani1 <saiani@naver.com>
2025-11-20 16:09:57 +0900

    blog, news폴더 layout을 main으로 통합

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,15 +1,31 @@
 import { Header } from "widgets/Header";
+import { ProfileAside } from "features/Profile";
+import { Category } from "features/Category";
+import { getCategories } from "entities/category";
+import { getProfileInfo, getProfileInfoJp } from "entities/user";
 
 export default async function MainLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  const profileData =
+    locale === "ko" ? await getProfileInfo() : await getProfileInfoJp();
+  const categoryData = await getCategories();
+
   return (
     <div className="flex flex-col items-center w-full">
       <Header />
       <div className="flex justify-between w-full lg:w-[80%] md:w-full sm:w-full mt-[10px] mb-0 md:mb-[40px] gap-x-[10px]">
-        {children}
+        <aside className="hidden lg:flex flex-col">
+          <ProfileAside data={profileData} />
+          {categoryData && <Category data={categoryData} />}
+        </aside>
+        <main className="sm:pt-[105px] pt-[40px] sm:px-[50px] pb-[40px] w-full h-auto min-h-screen bg-black-10 sm:rounded-xl">
+          {children}
+        </main>
       </div>
     </div>
   );

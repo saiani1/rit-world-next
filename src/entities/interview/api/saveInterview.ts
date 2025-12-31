7590c4c92b8ad9b59ad430b9fc887787d214de80
saiani1 <saiani@naver.com>
2025-12-31 20:12:58 +0900

    type명 수정

--- src/entities/interview/api/saveInterview.ts
+++ src/entities/interview/api/saveInterview.ts
@@ -1,7 +1,10 @@
-import { AnalysisResult } from "../model";
+import { AnalysisResultType } from "../model";
 import { supabase } from "shared/index";
 
-export const saveInterview = async (data: AnalysisResult, rawText: string) => {
+export const saveInterview = async (
+  data: AnalysisResultType,
+  rawText: string
+) => {
   const { error } = await supabase.from("interviews").insert({
     ...data,
     raw_text: rawText,

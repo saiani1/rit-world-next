f4cd883ddde806c0c5e0b6822e77928ebb6be56a
saiani1 <saiani@naver.com>
2026-02-10 13:55:02 +0900

    toast editor -> mdxeditor로 변경

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,11 +1,2 @@
 /* --- CSS Modules --- */
 declare module "*.css";
-declare module "prismjs/themes/prism.css";
-declare module "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
-
-/* --- PrismJS language components --- */
-declare module "prismjs/components/prism-javascript";
-declare module "prismjs/components/prism-typescript";
-declare module "prismjs/components/prism-java";
-declare module "prismjs/components/prism-python";
-declare module "prismjs/components/prism-c";

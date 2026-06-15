87601762805349b13b454f1dbb9fbe0823db6d9d
saiani1 <saiani@naver.com>
2026-06-15 17:25:16 +0900

    saveCompany api수정

--- src/entities/interview/api/saveCompany.ts
+++ src/entities/interview/api/saveCompany.ts
@@ -1,8 +1,18 @@
-import { supabase } from "shared/index";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+import { supabase as defaultSupabase } from "shared/index";
 import { CompanyTableType } from "../model";
 
-export const saveCompany = async (data: CompanyTableType) => {
-  const { error } = await supabase.from("companies").insert(data);
+export const saveCompany = async (
+  data: Partial<CompanyTableType>,
+  client?: SupabaseClient
+) => {
+  const supabaseClient = client || defaultSupabase;
+  const { data: insertedData, error } = await supabaseClient
+    .from("companies")
+    .insert(data)
+    .select();
 
   if (error) throw error;
+  return insertedData;
 };
